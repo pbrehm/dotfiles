817482e85e78087c1d2eed2d83e553800bfb0152
pbrehm <peter.brehm@outlook.com>
2023-09-21 14:23:29 -0500

    add colorscheme

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  -- add github theme
+  { "projekt0n/github-nvim-theme" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "github_dark_dimmed",
+      colorscheme = "tokyonight-night",
+    },
+  },
+}
