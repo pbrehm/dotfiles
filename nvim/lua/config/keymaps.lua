6989c118740d4e3d125de54bdfd51ea0248b358a
pbrehm <peter.brehm@outlook.com>
2023-09-24 17:07:17 -0500

    remap delete to void functions to use capital letter

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,15 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.api.nvim_set_keymap("n", "<leader>d", '"_d', { noremap = true })
-vim.api.nvim_set_keymap("x", "<leader>d", '"_d', { noremap = true })
-vim.api.nvim_set_keymap("x", "<leader>p", '"_dP', { noremap = true })
+-- originally I used <leader>d
+-- this conflicts with dap debug keybindings
+-- I seldom use D and it functions the same as d$
+-- remap D to delete to void
+vim.api.nvim_set_keymap("n", "D", '"_d', { noremap = true })
+vim.api.nvim_set_keymap("n", "DD", '"_dd', { noremap = true })
+
+vim.api.nvim_set_keymap("x", "D", '"_d', { noremap = true })
+vim.api.nvim_set_keymap("x", "DD", '"_dd', { noremap = true })
+
+-- remap P in visual mode to delete then paste to not pollute registers
+vim.api.nvim_set_keymap("x", "P", '"_dP', { noremap = true })
