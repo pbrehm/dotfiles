915108008768ede7e8e8c65b12edd247d3fb5ee3
pbrehm <peter.brehm@outlook.com>
2023-09-25 23:24:20 -0500

    add jest snippets

---
+++ nvim/lua/plugins/luasnip.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      -- TODO: figure out why "coco-platform/vscode-jest-snippets" works when "andys8/vscode-jest-snippets" doesn't
+      -- I see every andys8 snippet has a source = "source.js" but coco doesn't. 🤔
+
+      -- "andys8/vscode-jest-snippets",
+      "coco-platform/vscode-jest-snippets"
+    }
+  }
+}
