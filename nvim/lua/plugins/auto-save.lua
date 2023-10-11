4c65a291606512901b1efcd437504304f5d586e9
pbrehm <peter.brehm@outlook.com>
2023-10-11 11:37:33 -0500

    convert to using okuuva auto-save fork

--- nvim/lua/plugins/auto-save.lua
+++ nvim/lua/plugins/auto-save.lua
@@ -1,14 +1,17 @@
 return {
   {
-    "pocco81/auto-save.nvim",
-    config = function()
-      require('auto-save').setup({
-        execution_message = {
-          message = function() -- message no message on auto save
-            return ("")
-          end,
-        },
-      })
-    end
+    -- originally used pocco81/auto-save.nvim
+    -- now I use this fork. I like that it does a defer_save (save after deboune on insertLeave)
+    -- this is useful when adding a comment and leaving a space at the end with the intent to paste something after the space
+    -- without the defer_save, as soon as you exist insert mode, the space is wiped out
+    -- also more actively developed
+    "okuuva/auto-save.nvim",
+    opts = {
+      execution_message = {
+        message = function()   -- message no message on auto save
+          return ("")
+        end,
+      },
+    }
   }
 }
