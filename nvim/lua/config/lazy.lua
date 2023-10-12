7f2084d23123f5183412efbe4ad66b72b9460da6
pbrehm <peter.brehm@outlook.com>
2023-10-12 13:38:49 -0500

    remove un needed clipboard code from lazy.lua

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -7,47 +7,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
--- wsl clipboard support with WSLg
-if vim.fn.has("wsl") == 1 then
-  if vim.fn.executable("wl-copy") == 0 then
-    print("wl-clipboard not found, clipboard integration won't work")
-  else
-    vim.g.clipboard = {
-      name = "wl-clipboard (wsl)",
-      copy = {
-        ["+"] = "wl-copy --foreground --type text/plain",
-        ["*"] = "wl-copy --foreground --primary --type text/plain",
-      },
-      paste = {
-        ["+"] = function()
-          return vim.fn.systemlist('wl-paste --no-newline|sed -e "s/\r$//"', { "" }, 1) -- '1' keeps empty lines
-        end,
-        ["*"] = function()
-          return vim.fn.systemlist('wl-paste --primary --no-newline|sed -e "s/\r$//"', { "" }, 1)
-        end,
-      },
-      cache_enabled = true,
-    }
-  end
-end
-
--- wsl clipboard support for no WSLg
--- must have win32yank.exe in path
--- if vim.fn.has("wsl") == 1 then
---   vim.g.clipboard = {
---     name = "win32yank-wsl",
---     copy = {
---       ["+"] = "win32yank.exe -i --crlf",
---       ["*"] = "win32yank.exe -i --crlf",
---     },
---     paste = {
---       ["+"] = "win32yank.exe -o --lf",
---       ["*"] = "win32yank.exe -o --lf",
---     },
---     cache_enabled = true,
---   }
--- end
-
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
