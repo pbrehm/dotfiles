656b5459163d652d152c8668d18212a7a1c74437
pbrehm <peter.brehm@outlook.com>
2023-10-14 22:37:10 -0500

    initial setup for nvim setup
    setup nvim.kickstart but break all plugins, options and keymaps into folders

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'hrsh7th/nvim-cmp',
+    -- Autocompletion
+    dependencies = {
+      -- Snippet Engine & its associated nvim-cmp source
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+
+      -- Adds LSP completion capabilities
+      'hrsh7th/cmp-nvim-lsp',
+
+      -- Adds a number of user-friendly snippets
+      'rafamadriz/friendly-snippets',
+    },
+  },
+}
---
+++ nvim/lua/plugins/tpope.lua
@@ -0,0 +1,10 @@
+return {
+  -- NOTE: First, some plugins that don't require any configuration
+
+  -- Git related plugins
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',
+
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
+}
