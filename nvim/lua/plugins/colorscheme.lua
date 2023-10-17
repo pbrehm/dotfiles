9f09e307be58d70da46ac05ea537afe68021fc08
pbrehm <peter.brehm@outlook.com>
2023-10-17 14:53:00 -0500

    configure colorscheme and lualin theme to tokyonight

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,9 @@
 return {
   {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    "folke/tokyonight.nvim",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   opts = {},
-  --   config = function()
-  --     vim.cmd.colorscheme 'tokyonight'
-  --   end,
-  -- }
 }
