6842d7f56e4ad1b29add6dfd78f8bed0470d025b
pbrehm <peter.brehm@outlook.com>
2023-10-17 23:17:19 -0500

    add autocmd as workaround for weird gitsign diff behavior

--- lazyvim/lua/config/autocmds.lua
+++ lazyvim/lua/config/autocmds.lua
@@ -1,3 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+--when exiting gitsign. if you exit the wrong window signcolumn gets set to "no"
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "",
+  callback = function()
+    if vim.bo.buftype == "" then
+      vim.wo.signcolumn = "yes"
+      -- vim.schedule(function()
+      -- print("signcolumn set to yes")
+      -- end)
+    end
+  end,
+})
