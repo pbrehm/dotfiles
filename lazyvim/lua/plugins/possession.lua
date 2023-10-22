78cc582f51ca83d1824c1fec652d3da1fbc001d8
pbrehm <peter.brehm@outlook.com>
2023-10-22 00:27:58 -0500

    possession remap telescope keys

--- lazyvim/lua/plugins/possession.lua
+++ lazyvim/lua/plugins/possession.lua
@@ -13,5 +13,16 @@ return {
     autosave = {
       current = true,
     },
+    telescope = {
+      list = {
+        default_action = "load",
+        mappings = {
+          save = { n = "<c-s>", i = "<c-s>" },
+          load = { n = "<c-l>", i = "<c-l>" },
+          delete = { n = "<c-d>", i = "<c-d>" },
+          rename = { n = "<c-r>", i = "<c-r>" },
+        },
+      },
+    },
   },
 }
