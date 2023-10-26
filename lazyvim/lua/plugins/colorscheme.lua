9bf9236eda399423bde2119d9a3f51a26fc30c21
pbrehm <peter.brehm@outlook.com>
2023-10-26 14:05:09 -0500

    experiment
    unable to change focused tab to have same color as others so ditching it

--- lazyvim/lua/plugins/colorscheme.lua
+++ lazyvim/lua/plugins/colorscheme.lua
@@ -2,12 +2,44 @@ return {
   -- add github theme
   { "projekt0n/github-nvim-theme" },
 
-  -- Configure LazyVim to load gruvbox
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    -- opts = {
+    --   color_overrides = {
+    --     mocha = {
+    --       base = "#0c0c13",
+    --       mantle = "#07070b",
+    --       crust = "#0d0d0d",
+    --     },
+    --   },
+    -- },
+  },
+  -- { "dasupradyumna/midnight.nvim" },
+  -- { "bluz71/vim-moonfly-colors", name = "moonfly" },
+  {
+    "navarasu/onedark.nvim",
+    opts = {
+      -- style = "deep",
+      style = "darker",
+      -- colors = {
+      -- black = "#000000",
+      -- bg0 = "#111111",
+      -- bg1 = "#1a1a1a",
+      -- bg2 = "#000000",
+      -- bg3 = "#000000",
+      -- bg_d = "#111111",
+      -- },
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "github_dark_dimmed",
       colorscheme = "tokyonight-night",
+      -- colorscheme = "moonfly",
+      -- colorscheme = "catppuccin",
+      -- colorscheme = "rose-pine"
     },
   },
 }
--- lazyvim/lua/plugins/lualine.lua
+++ lazyvim/lua/plugins/lualine.lua
@@ -3,6 +3,11 @@ return {
     "nvim-lualine/lualine.nvim",
     -- disable clock since tmux has it
     opts = {
+      options = {
+        -- section_separators = { left = "█", right = "" },
+        section_separators = { left = "█", right = "█" },
+        component_separators = "|",
+      },
       sections = {
         lualine_z = {},
       },
