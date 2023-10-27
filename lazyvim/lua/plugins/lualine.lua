c56c6adab9f68f818b2c477c4d5a5b276d02b77d
pbrehm <peter.brehm@outlook.com>
2023-10-27 10:35:28 -0500

    revert to simple tmux line and simple lualine

--- lazyvim/lua/plugins/lualine.lua
+++ lazyvim/lua/plugins/lualine.lua
@@ -8,8 +8,8 @@ return {
     -- disable clock since tmux has it
     opts = {
       options = {
-        section_separators = { left = "", right = "" },
-        -- section_separators = { left = "", right = "" },
+        -- section_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
         component_separators = "|",
       },-- █
       sections = {
