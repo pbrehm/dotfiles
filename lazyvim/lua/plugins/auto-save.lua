6c6cef652ffd25c583c925708f3a331ba2f1c596
pbrehm <peter.brehm@outlook.com>
2023-11-12 21:15:57 -0600

    lazyvim: fix auto save to actually load. then disable it

--- lazyvim/lua/plugins/auto-save.lua
+++ lazyvim/lua/plugins/auto-save.lua
@@ -1,23 +1,23 @@
 return {
-  {
-    -- originally used pocco81/auto-save.nvim
-    -- now I use this fork. I like that it does a defer_save (save after deboune on insertLeave)
-    -- this is useful when adding a comment and leaving a space at the end with the intent to paste something after the space
-    -- without the defer_save, as soon as you exist insert mode, the space is wiped out
-    -- also more actively developed
-    -- "okuuva/auto-save.nvim",
-    -- opts = {
-    --   execution_message = {
-    --     message = function() -- message no message on auto save
-    --       return ""
-    --     end,
-    --   },
-    --   -- this way if we do a refactor changes in other buffers will get auto saved
-    --   write_all_buffers = true,
-    -- },
-    -- lazy = false,
-    -- keys = {
-    --   { "<leader>ua", vim.cmd.ASToggle, desc = "Toggle Auto Save" },
-    -- },
-  },
+  -- {
+  --   -- originally used pocco81/auto-save.nvim
+  --   -- now I use this fork. I like that it does a defer_save (save after deboune on insertLeave)
+  --   -- this is useful when adding a comment and leaving a space at the end with the intent to paste something after the space
+  --   -- without the defer_save, as soon as you exist insert mode, the space is wiped out
+  --   -- also more actively developed
+  --   "okuuva/auto-save.nvim",
+  --   lazy = false,
+  --   opts = {
+  --     execution_message = {
+  --       message = function() -- message no message on auto save
+  --         return ""
+  --       end,
+  --     },
+  --     -- this way if we do a refactor changes in other buffers will get auto saved
+  --     write_all_buffers = true,
+  --   },
+  --   keys = {
+  --     { "<leader>ua", vim.cmd.ASToggle, desc = "Toggle Auto Save" },
+  --   },
+  -- },
 }
