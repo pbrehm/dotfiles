b7126489e435c52f5957b6a6c9eb433964fafbe3
pbrehm <peter.brehm@outlook.com>
2023-11-12 20:55:50 -0600

    change curslintopt to both instead of number
    both highlight the number and the line.
    the default it just the line and no line numbers

--- lazyvim/lua/plugins/neo-tree.lua
+++ lazyvim/lua/plugins/neo-tree.lua
@@ -6,7 +6,7 @@ return {
         event = "vim_buffer_enter",
         handler = function()
           if vim.bo.filetype == "neo-tree" then
-            vim.cmd([[setlocal number relativenumber cursorlineopt=number]])
+            vim.cmd([[setlocal number relativenumber cursorlineopt=both]])
           end
         end,
       },
