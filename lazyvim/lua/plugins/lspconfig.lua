070376e547f337e3f33f4f81509b0e75b4fc6c24
pbrehm <peter.brehm@outlook.com>
2023-11-14 14:55:31 -0600

    disable lazy typescript. manually configure typescript for treesitter and dap, configure lspconfig for vtsls

---
+++ lazyvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,56 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      vtsls = {
+        keys = {
+          {
+            "<leader>co",
+            function()
+              vim.lsp.buf.code_action({
+                apply = true,
+                context = {
+                  only = { "source.organizeImports" },
+                  diagnostics = {},
+                },
+              })
+            end,
+            desc = "Organize Imports",
+          },
+          {
+            "<leader>cR",
+            function()
+              vim.lsp.buf.code_action({
+                apply = true,
+                context = {
+                  only = { "source.removeUnusedImports" },
+                  diagnostics = {},
+                },
+              })
+            end,
+            desc = "Remove Unused Imports",
+          },
+        },
+        settings = {
+          typescript = {
+            format = {
+              indentSize = vim.o.shiftwidth,
+              convertTabsToSpaces = vim.o.expandtab,
+              tabSize = vim.o.tabstop,
+            },
+          },
+          javascript = {
+            format = {
+              indentSize = vim.o.shiftwidth,
+              convertTabsToSpaces = vim.o.expandtab,
+              tabSize = vim.o.tabstop,
+            },
+          },
+          completions = {
+            completeFunctionCalls = true,
+          },
+        },
+      },
+    },
+  },
+}
