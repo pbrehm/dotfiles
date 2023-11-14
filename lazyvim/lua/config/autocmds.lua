8a138bc034ad65e789fd1d4cd28a061ed6543dab
pbrehm <peter.brehm@outlook.com>
2023-11-14 11:14:42 -0600

    re enable autocmd to foce signcolumn on

--- lazyvim/lua/config/autocmds.lua
+++ lazyvim/lua/config/autocmds.lua
@@ -3,14 +3,14 @@
 -- Add any additional autocmds here
 
 --when exiting gitsign. if you exit the wrong window signcolumn gets set to "no"
--- vim.api.nvim_create_autocmd("BufEnter", {
---   pattern = "",
---   callback = function()
---     if vim.bo.buftype == "" then
---       vim.wo.signcolumn = "yes"
---       -- vim.schedule(function()
---       -- print("signcolumn set to yes")
---       -- end)
---     end
---   end,
--- })
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "",
+  callback = function()
+    if vim.bo.buftype == "" then
+      vim.wo.signcolumn = "yes"
+      -- vim.schedule(function()
+      -- print("signcolumn set to yes")
+      -- end)
+    end
+  end,
+})
