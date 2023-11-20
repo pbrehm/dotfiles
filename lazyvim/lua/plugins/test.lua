6db4a1406ece4ea567ccc81f184e8a258fead4c0
pbrehm <peter.brehm@outlook.com>
2023-11-20 15:34:31 -0600

    setup cwd for neo-test-jest to work in nested repo (/app/package.json)

--- lazyvim/lua/plugins/test.lua
+++ lazyvim/lua/plugins/test.lua
@@ -33,7 +33,23 @@ return {
           jestCommand = "npm test --",
           jestConfigFile = "custom.jest.config.ts",
           env = { CI = true },
+          -- default just use cwd
+          -- cwd = function()
+          --   return vim.fn.getcwd()
+          -- end,
+
+          -- nested repo setup
           cwd = function()
+            -- Retrieve the full path of the current file being edited in Vim
+            local file = vim.fn.expand("%:p")
+
+            -- Check if the file path contains "/app/"
+            if string.find(file, "/app/") then
+              -- If the file path contains "/app/", return "/app/" as the working directory
+              return string.match(file, "(.*/app/)")
+            end
+
+            -- If the file path doesn't contain "/app/", return the current working directory in Vim
             return vim.fn.getcwd()
           end,
         })
