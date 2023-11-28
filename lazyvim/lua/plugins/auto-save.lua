9e1329024a90c6f71d6063cab057756846e10d50
pbrehm <peter.brehm@outlook.com>
2023-11-28 11:43:30 -0600

    fix auto save to load on startup and also disable it

--- lazyvim/lua/plugins/auto-save.lua
+++ lazyvim/lua/plugins/auto-save.lua
@@ -5,18 +5,19 @@ return {
     -- this is useful when adding a comment and leaving a space at the end with the intent to paste something after the space
     -- without the defer_save, as soon as you exist insert mode, the space is wiped out
     -- also more actively developed
-    "okuuva/auto-save.nvim",
-    opts = {
-      execution_message = {
-        message = function() -- message no message on auto save
-          return ""
-        end,
-      },
-      -- this way if we do a refactor changes in other buffers will get auto saved
-      write_all_buffers = true,
-    },
-    keys = {
-      { "<leader>ua", vim.cmd.ASToggle, desc = "Toggle Auto Save" },
-    },
+    -- "okuuva/auto-save.nvim",
+    -- opts = {
+    --   execution_message = {
+    --     message = function() -- message no message on auto save
+    --       return ""
+    --     end,
+    --   },
+    --   -- this way if we do a refactor changes in other buffers will get auto saved
+    --   write_all_buffers = true,
+    -- },
+    -- lazy = false,
+    -- keys = {
+    --   { "<leader>ua", vim.cmd.ASToggle, desc = "Toggle Auto Save" },
+    -- },
   },
 }
