90ee92fa051b7b19aa77a50d5f54db2ed85f585b
pbrehm <peter.brehm@outlook.com>
2024-02-09 13:17:32 -0600

    set static commit for treesitter and match-up because treesitter breaks certain plugins
    match-up has fix, but neotest jest plugin needs to be updated and has not been yet.

--- lazyvim/lua/plugins/match-up.lua
+++ lazyvim/lua/plugins/match-up.lua
@@ -1,6 +1,7 @@
 return {
   {
     "andymass/vim-matchup",
+    commit = "e2cca1747ab175b8d839a5d28679427564643a57"
     -- disable clock since tmux has it
   },
   -- enable treesitter support for matchup
