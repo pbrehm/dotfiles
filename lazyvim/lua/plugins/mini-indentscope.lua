76018e757f49942223184542798e198bb89934b1
pbrehm <peter.brehm@outlook.com>
2024-02-14 13:31:21 -0600

    lazyvim: disable animation for mini.indentscope

---
+++ lazyvim/lua/plugins/mini-indentscope.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "echasnovski/mini.indentscope",
+    opts = {
+      -- options = {
+      --   indent_at_cursor = false,
+      -- },
+      draw = {
+        delay = 0,
+        animation = require('mini.indentscope').gen_animation.none()
+      }
+    }
+  }
+}
