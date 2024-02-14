55db434d3a297a08f2b14c0bbeb93e15e6dfb6e0
pbrehm <peter.brehm@outlook.com>
2024-02-14 14:50:12 -0600

    nvim: send pasted over text to void buffer so that I don't lose what I'm pasting

--- lazyvim/lua/config/keymaps.lua
+++ lazyvim/lua/config/keymaps.lua
@@ -5,15 +5,17 @@
 -- this conflicts with dap debug keybindings
 -- I seldom use D and it functions the same as d$
 -- remap D to delete to void
-vim.api.nvim_set_keymap("n", "D", '"_d', { noremap = true })
-vim.api.nvim_set_keymap("n", "DD", '"_dd', { noremap = true })
+-- vim.api.nvim_set_keymap("n", "D", '"_d', { noremap = true })
+-- vim.api.nvim_set_keymap("n", "DD", '"_dd', { noremap = true })
 
-vim.api.nvim_set_keymap("x", "D", '"_d', { noremap = true })
-vim.api.nvim_set_keymap("x", "DD", '"_dd', { noremap = true })
+-- vim.api.nvim_set_keymap("x", "D", '"_d', { noremap = true })
+-- vim.api.nvim_set_keymap("x", "DD", '"_dd', { noremap = true })
 
 -- remap P in visual mode to delete then paste to not pollute registers
-vim.api.nvim_set_keymap("x", "P", '"_dP', { noremap = true })
+-- vim.api.nvim_set_keymap("x", "P", '"_dP', { noremap = true })
 
+-- send pasted over text to void buffer so that I don't lose what I'm pasting
+vim.api.nvim_set_keymap("x", "p", [["_dP]], { noremap = true, silent = true })
 
 -- when when pressing escape to exit insert mode sometimes its easy to pres esc+j which is the same as alt+J
 -- this results in moving the line. I just unbind these in insert and visual mode
