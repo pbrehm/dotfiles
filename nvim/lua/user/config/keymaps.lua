88e473971ccae7846afd69c0b37f90c477c0798a
pbrehm <peter.brehm@outlook.com>
2024-02-15 14:05:57 -0600

    nvim: copy lazyvim keybinds and comment out ones I don't like with TODO to fix ones I do

--- nvim/lua/user/config/keymaps.lua
+++ nvim/lua/user/config/keymaps.lua
@@ -1,47 +1,201 @@
-local keymap = vim.keymap.set
-local opts = { noremap = true, silent = true }
+local map = vim.keymap.set
 
-keymap("n", "<Space>", "", opts)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+-- better up/down
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
-keymap("n", "<C-i>", "<C-i>", opts)
+-- Move to window using the <ctrl> hjkl keys
+map("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
 
--- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
--- keymap("n", "<m-tab>", "<c-6>", opts)
+-- Resize window using <ctrl> arrow keys
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 
-keymap("n", "n", "nzz", opts)
-keymap("n", "N", "Nzz", opts)
-keymap("n", "*", "*zz", opts)
-keymap("n", "#", "#zz", opts)
-keymap("n", "g*", "g*zz", opts)
-keymap("n", "g#", "g#zz", opts)
+-- Move Lines -- 
+-- when when pressing escape to exit insert mode sometimes its easy to pres esc+j which is the same as alt+J
+-- this results in moving the line. I just unbind these in insert and visual mode
+-- since I only need to move lines in normal mode
+-- map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+-- map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+-- map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+-- map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+-- map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+-- map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 
--- Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+-- buffers
+-- no need since I use CTRL-o and CTRL-i
+-- map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+-- map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+-- map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+-- map("n", "]b", "<cmd>bnext<cr>", { desc = "Next buffer" })
+-- map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+-- map("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
 
-keymap("x", "p", [["_dP]])
+-- Clear search with <esc>
+-- map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
 
-vim.cmd [[:amenu 10.100 mousemenu.Goto\ Definition <cmd>lua vim.lsp.buf.definition()<CR>]]
-vim.cmd [[:amenu 10.110 mousemenu.References <cmd>lua vim.lsp.buf.references()<CR>]]
--- vim.cmd [[:amenu 10.120 mousemenu.-sep- *]]
+-- Clear search, diff update and redraw
+-- taken from runtime/lua/_editor.lua
+-- map(
+--   "n",
+--   "<leader>ur",
+--   "<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>",
+--   { desc = "Redraw / clear hlsearch / diff update" }
+-- )
 
-vim.keymap.set("n", "<RightMouse>", "<cmd>:popup mousemenu<CR>")
-vim.keymap.set("n", "<Tab>", "<cmd>:popup mousemenu<CR>")
+-- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+-- then center afterwords
+map("n", "n", "'Nn'[v:searchforward].'zvzz'", { expr = true, desc = "Next search result" })
+map("x", "n", "'Nn'[v:searchforward].'zz'", { expr = true, desc = "Next search result" })
+map("o", "n", "'Nn'[v:searchforward].'zz'", { expr = true, desc = "Next search result" })
+map("n", "N", "'nN'[v:searchforward].'zvzz'", { expr = true, desc = "Prev search result" })
+map("x", "N", "'nN'[v:searchforward].'zz'", { expr = true, desc = "Prev search result" })
+map("o", "N", "'nN'[v:searchforward].'zz'", { expr = true, desc = "Prev search result" })
 
--- more good
-keymap({ "n", "o", "x" }, "<s-h>", "^", opts)
-keymap({ "n", "o", "x" }, "<s-l>", "g_", opts)
+-- Add undo break-points
+-- map("i", ",", ",<c-g>u")
+-- map("i", ".", ".<c-g>u")
+-- map("i", ";", ";<c-g>u")
 
--- tailwind bearable to work with
-keymap({ "n", "x" }, "j", "gj", opts)
-keymap({ "n", "x" }, "k", "gk", opts)
-keymap("n", "<leader>w", ":lua vim.wo.wrap = not vim.wo.wrap<CR>", opts)
+-- save file
+-- map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+
+--keywordprg
+-- map("n", "<leader>K", "<cmd>norm! K<cr>", { desc = "Keywordprg" })
+
+-- better indenting
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+
+-- lazy
+map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
+
+-- new file
+-- map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
+
+-- map("n", "<leader>xl", "<cmd>lopen<cr>", { desc = "Location List" })
+-- map("n", "<leader>xq", "<cmd>copen<cr>", { desc = "Quickfix List" })
+
+-- map("n", "[q", vim.cmd.cprev, { desc = "Previous quickfix" })
+-- map("n", "]q", vim.cmd.cnext, { desc = "Next quickfix" })
+
+-- TODO: fix code format keybdind
+-- formatting
+-- map({ "n", "v" }, "<leader>cf", function()
+--   Util.format({ force = true })
+-- end, { desc = "Format" })
+
+-- diagnostic
+local diagnostic_goto = function(next, severity)
+  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+  severity = severity and vim.diagnostic.severity[severity] or nil
+  return function()
+    go({ severity = severity })
+  end
+end
+map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+map("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
+map("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
+map("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
+map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
+map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
+map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
+
+-- stylua: ignore start
+
+-- toggle options
+-- TODO: revisit ui toggles
+-- map("n", "<leader>uf", function() Util.format.toggle() end, { desc = "Toggle auto format (global)" })
+-- map("n", "<leader>uF", function() Util.format.toggle(true) end, { desc = "Toggle auto format (buffer)" })
+-- map("n", "<leader>us", function() Util.toggle("spell") end, { desc = "Toggle Spelling" })
+-- map("n", "<leader>uw", function() Util.toggle("wrap") end, { desc = "Toggle Word Wrap" })
+-- map("n", "<leader>uL", function() Util.toggle("relativenumber") end, { desc = "Toggle Relative Line Numbers" })
+-- map("n", "<leader>ul", function() Util.toggle.number() end, { desc = "Toggle Line Numbers" })
+-- map("n", "<leader>ud", function() Util.toggle.diagnostics() end, { desc = "Toggle Diagnostics" })
 
 
-vim.api.nvim_set_keymap('t', '<C-;>', '<C-\\><C-n>', opts)
+
+-- local conceallevel = vim.o.conceallevel > 0 and vim.o.conceallevel or 3
+-- map("n", "<leader>uc", function() Util.toggle("conceallevel", false, { 0, conceallevel }) end,
+--   { desc = "Toggle Conceal" })
+
+-- TODO: consider adding toggle inlay hint back
+-- if vim.lsp.buf.inlay_hint or vim.lsp.inlay_hint then
+--   map("n", "<leader>uh", function() Util.toggle.inlay_hints() end, { desc = "Toggle Inlay Hints" })
+-- end
+
+-- map("n", "<leader>uT", function() if vim.b.ts_highlight then vim.treesitter.stop() else vim.treesitter.start() end end,
+--   { desc = "Toggle Treesitter Highlight" })
+
+-- map("n", "<leader>ub", function() Util.toggle("background", false, { "light", "dark" }) end,
+--   { desc = "Toggle Background" })
+
+-- lazygit
+-- TODO: bring lazygit back
+-- map("n", "<leader>gg",
+--   function() Util.terminal({ "lazygit" }, { cwd = Util.root(), esc_esc = false, ctrl_hjkl = false }) end,
+--   { desc = "Lazygit (root dir)" })
+-- map("n", "<leader>gG", function() Util.terminal({ "lazygit" }, { esc_esc = false, ctrl_hjkl = false }) end,
+--   { desc = "Lazygit (cwd)" })
+
+-- quit
+-- map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
+
+-- highlights under cursor
+-- map("n", "<leader>ui", vim.show_pos, { desc = "Inspect Pos" })
+
+-- LazyVim Changelog
+-- map("n", "<leader>L", function() Util.news.changelog() end, { desc = "LazyVim Changelog" })
+
+-- floating terminal
+-- local lazyterm = function() Util.terminal(nil, { cwd = Util.root() }) end
+-- map("n", "<leader>ft", lazyterm, { desc = "Terminal (root dir)" })
+-- map("n", "<leader>fT", function() Util.terminal() end, { desc = "Terminal (cwd)" })
+-- map("n", "<c-/>", lazyterm, { desc = "Terminal (root dir)" })
+-- map("n", "<c-_>", lazyterm, { desc = "which_key_ignore" })
+
+-- Terminal Mappings
+-- map("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
+-- map("t", "<C-h>", "<cmd>wincmd h<cr>", { desc = "Go to left window" })
+-- map("t", "<C-j>", "<cmd>wincmd j<cr>", { desc = "Go to lower window" })
+-- map("t", "<C-k>", "<cmd>wincmd k<cr>", { desc = "Go to upper window" })
+-- map("t", "<C-l>", "<cmd>wincmd l<cr>", { desc = "Go to right window" })
+-- map("t", "<C-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+-- map("t", "<c-_>", "<cmd>close<cr>", { desc = "which_key_ignore" })
+
+-- windows
+-- map("n", "<leader>ww", "<C-W>p", { desc = "Other window", remap = true })
+-- map("n", "<leader>wd", "<C-W>c", { desc = "Delete window", remap = true })
+-- map("n", "<leader>w-", "<C-W>s", { desc = "Split window below", remap = true })
+-- map("n", "<leader>w|", "<C-W>v", { desc = "Split window right", remap = true })
+map("n", "<leader>-", "<C-W>s", { desc = "Split window below", remap = true })
+map("n", "<leader>|", "<C-W>v", { desc = "Split window right", remap = true })
+
+-- tabs
+map("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
+map("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First Tab" })
+map("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
+map("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+map("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
+map("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+
+
+-- personal keybinds
+--
+-- send pasted over text to void buffer so that I don't lose what I'm pasting
+vim.api.nvim_set_keymap("x", "p", [["_dP]], { noremap = true, silent = true })
+
+-- center automatically when looking through highlighted searches
+-- vim.api.nvim_set_keymap("n", "n", "nzz", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "N", "Nzz", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "*", "*zz", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "#", "#zz", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "g*", "g*zz", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "g#", "g#zz", { noremap = true, silent = true })
