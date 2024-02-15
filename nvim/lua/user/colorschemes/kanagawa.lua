37d3305d5e79fa163db3f86b8aa0faf318a793ec
pbrehm <peter.brehm@outlook.com>
2024-02-15 14:13:02 -0600

    nvim: setup empty init.lua with just autocmds, options, keymaps, lazy, colorscheme

---
+++ nvim/lua/user/colorschemes/kanagawa.lua
@@ -0,0 +1,24 @@
+local M = {
+  "rebelot/kanagawa.nvim",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+}
+
+function M.config()
+  require("kanagawa").setup {
+    colors = {
+      theme = {
+        wave = {
+          ui = {
+            -- override gutter to be same as bg
+            bg_gutter = "none",
+          },
+        },
+      },
+    },
+    terminalColors = false,
+  }
+  vim.cmd.colorscheme "kanagawa-wave"
+end
+
+return M
