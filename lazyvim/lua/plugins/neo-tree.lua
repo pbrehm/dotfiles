dd5b2d98b1b6a63a30379ec88cc8fbe33287ff61
pbrehm <peter.brehm@outlook.com>
2024-02-16 12:07:27 -0600

    lazyvim: switch <leader>fE and <leader>fe as well as <leader>e and <leader>E
    I want the default behavior when I open the explorer to open to CWD. this is the directory I opened neovim in.
    the "root" is like the project root. so if I'm inside a subprojected in a monorepo is will open that instead.
    
    I still want this functionality, but more often then not I want to open the CWD

--- lazyvim/lua/plugins/neo-tree.lua
+++ lazyvim/lua/plugins/neo-tree.lua
@@ -1,3 +1,5 @@
+local Util = require("lazyvim.util")
+
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
@@ -12,4 +14,22 @@ return {
       },
     },
   },
+  keys = {
+    {
+      "<leader>fE",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = Util.root() })
+      end,
+      desc = "Explorer NeoTree (root dir)",
+    },
+    {
+      "<leader>fe",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+      end,
+      desc = "Explorer NeoTree (cwd)",
+    },
+    { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (cwd)", remap = true },
+    { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (root dir)",      remap = true },
+  }
 }
