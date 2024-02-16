26bd4e9a3b223e92b5500bbc9002af486762ab70
pbrehm <peter.brehm@outlook.com>
2024-02-16 12:08:42 -0600

    lazyvim: change <leader>/ to grep cwd instead of root.
    root has lesser scope then CWD. CWD is the dir I open nvim in.

--- lazyvim/lua/plugins/telescope.lua
+++ lazyvim/lua/plugins/telescope.lua
@@ -1,5 +1,10 @@
+local Util = require("lazyvim.util")
+
 return {
-  -- "nvim-telescope/telescope.nvim",
+  "nvim-telescope/telescope.nvim",
+  keys = {
+    { "<leader>/", Util.telescope("live_grep", { cwd = false }), desc = "Grep (cwd)" },
+  }
   -- config = function()
   --     require("telescope").load_extension("possession")
   -- end,
