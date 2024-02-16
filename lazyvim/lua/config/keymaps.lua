1766a78efed3301cc6cefc34d8273fb9918c9a57
pbrehm <peter.brehm@outlook.com>
2024-02-16 12:05:18 -0600

    lazyvim: update keymaps. enhance lazyvim search iteration, disable shift h and l for buffer switching

--- lazyvim/lua/config/keymaps.lua
+++ lazyvim/lua/config/keymaps.lua
@@ -14,22 +14,28 @@
 -- remap P in visual mode to delete then paste to not pollute registers
 -- vim.api.nvim_set_keymap("x", "P", '"_dP', { noremap = true })
 
+
+local map = vim.api.nvim_set_keymap
 -- send pasted over text to void buffer so that I don't lose what I'm pasting
-vim.api.nvim_set_keymap("x", "p", [["_dP]], { noremap = true, silent = true })
+map("x", "p", [["_dP]], { noremap = true, silent = true })
 
 -- when when pressing escape to exit insert mode sometimes its easy to pres esc+j which is the same as alt+J
 -- this results in moving the line. I just unbind these in insert and visual mode
 -- since I only need to move lines in normal mode
-vim.api.nvim_set_keymap("i", "<A-j>", "", { desc = "Move down" })
-vim.api.nvim_set_keymap("i", "<A-k>", "", { desc = "Move up" })
-vim.api.nvim_set_keymap("v", "<A-j>", "", { desc = "Move down" })
-vim.api.nvim_set_keymap("v", "<A-k>", "", { desc = "Move up" })
+map("i", "<A-j>", "", { desc = "Move down DISABLED" })
+map("i", "<A-k>", "", { desc = "Move up DISABLED" })
+map("v", "<A-j>", "", { desc = "Move down DISABLED" })
+map("v", "<A-k>", "", { desc = "Move up DISABLED" })
+
+-- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+-- then center afterwords
+map("n", "n", "'Nn'[v:searchforward].'zvzz'", { expr = true, desc = "Next search result" })
+map("x", "n", "'Nn'[v:searchforward].'zz'", { expr = true, desc = "Next search result" })
+map("o", "n", "'Nn'[v:searchforward].'zz'", { expr = true, desc = "Next search result" })
+map("n", "N", "'nN'[v:searchforward].'zvzz'", { expr = true, desc = "Prev search result" })
+map("x", "N", "'nN'[v:searchforward].'zz'", { expr = true, desc = "Prev search result" })
+map("o", "N", "'nN'[v:searchforward].'zz'", { expr = true, desc = "Prev search result" })
 
 
--- center automatically when looking through highlighted searches
-vim.api.nvim_set_keymap("n", "n", "nzz", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "N", "Nzz", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "*", "*zz", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "#", "#zz", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "g*", "g*zz", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "g#", "g#zz", { noremap = true, silent = true })
+map("n", "<S-h>", "", { desc = "Prev buffer DISABLED" })
+map("n", "<S-l>", "", { desc = "Next buffer DISABLED" })
