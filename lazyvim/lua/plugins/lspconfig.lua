249fad008431bb72f93e41c801ab97a9066c0b45
pbrehm <peter.brehm@outlook.com>
2024-02-16 11:43:13 -0600

    lazyvim: add nvim-vtsls and use those functions to fix organize imports
    TODO: read through code of plugin to figure out how to implement myself

--- lazyvim/lua/plugins/lspconfig.lua
+++ lazyvim/lua/plugins/lspconfig.lua
@@ -1,33 +1,38 @@
 return {
   "neovim/nvim-lspconfig",
+  dependencies = {
+    "yioneko/nvim-vtsls",
+  },
   opts = {
     servers = {
       vtsls = {
         keys = {
           {
             "<leader>co",
-            function()
-              vim.lsp.buf.code_action({
-                apply = true,
-                context = {
-                  only = { "source.organizeImports" },
-                  diagnostics = {},
-                },
-              })
-            end,
+            -- function()
+            --   vim.lsp.buf.code_action({
+            --     apply = true,
+            --     context = {
+            --       only = { "source.organizeImports" },
+            --       diagnostics = {},
+            --     },
+            --   })
+            -- end,
+            "<cmd>VtsExec organize_imports<cr>",
             desc = "Organize Imports",
           },
           {
             "<leader>cR",
-            function()
-              vim.lsp.buf.code_action({
-                apply = true,
-                context = {
-                  only = { "source.removeUnusedImports" },
-                  diagnostics = {},
-                },
-              })
-            end,
+            -- function()
+            --   vim.lsp.buf.code_action({
+            --     apply = true,
+            --     context = {
+            --       only = { "source.removeUnusedImports" },
+            --       diagnostics = {},
+            --     },
+            --   })
+            -- end,
+            "<cmd>VtsExec remove_unused_imports<cr>",
             desc = "Remove Unused Imports",
           },
         },
