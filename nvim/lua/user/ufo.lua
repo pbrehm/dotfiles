068113087266cb8d68ca4bc347ed6b3bdca18b54
pbrehm <peter.brehm@outlook.com>
2024-02-20 16:13:40 -0600

    nvim: fix ufo so lspconfig isn't broken by it
    lsp ON_ATTACH function was not working with the previous setup so I just stole what launch.nvim had for the provider

--- nvim/lua/user/ufo.lua
+++ nvim/lua/user/ufo.lua
@@ -14,22 +14,6 @@ function M.config()
   -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
   vim.keymap.set("n", "zR", require("ufo").openAllFolds)
   vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
-  -- Option 2: nvim lsp as LSP client
-  -- Tell the server the capability of foldingRange,
-  -- Neovim hasn't added foldingRange to default capabilities, users must add it manually
-
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities.textDocument.foldingRange = {
-    dynamicRegistration = false,
-    lineFoldingOnly = true,
-  }
-  local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
-  for _, ls in ipairs(language_servers) do
-    require("lspconfig")[ls].setup {
-      capabilities = capabilities,
-      -- you can add other fields for setting up lsp server in this table
-    }
-  end
 
   local handler = function(virtText, lnum, endLnum, width, truncate)
     local newVirtText = {}
@@ -59,8 +43,22 @@ function M.config()
     return newVirtText
   end
 
+  local ftMap = {
+    typescript = { "lsp", "treesitter" },
+    -- python = { "indent" },
+    -- git = "",
+  }
+
   require("ufo").setup {
     fold_virt_text_handler = handler,
+    provider_selector = function(bufnr, filetype, buftype)
+      -- if you prefer treesitter provider rather than lsp,
+      -- return ftMap[filetype] or {'treesitter', 'indent'}
+      return ftMap[filetype]
+      -- return { "treesitter", "indent" }
+
+      -- refer to ./doc/example.lua for detail
+    end,
   }
 end
 
