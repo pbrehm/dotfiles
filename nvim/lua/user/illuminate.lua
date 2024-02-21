79929e455fe2fd35f9c0416458423bc06b9e1a5e
pbrehm <peter.brehm@outlook.com>
2024-02-21 11:46:13 -0600

    nvim: add [[ and ]] binding to illuminate to iterate through "illuminated" things

--- nvim/lua/user/illuminate.lua
+++ nvim/lua/user/illuminate.lua
@@ -5,6 +5,11 @@ local M = {
 
 function M.config()
   require("illuminate").configure {
+    delay = 200,
+    large_file_cutoff = 2000,
+    large_file_overrides = {
+      providers = { "lsp" },
+    },
     filetypes_denylist = {
       "mason",
       "harpoon",
@@ -31,6 +36,29 @@ function M.config()
       "TelescopePrompt",
     },
   }
+
+  local function map(key, dir, buffer)
+    vim.keymap.set("n", key, function()
+      require("illuminate")["goto_" .. dir .. "_reference"](false)
+    end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
+  end
+
+  map("]]", "next")
+  map("[[", "prev")
+
+  -- also set it after loading ftplugins, since a lot overwrite [[ and ]]
+  vim.api.nvim_create_autocmd("FileType", {
+    callback = function()
+      local buffer = vim.api.nvim_get_current_buf()
+      map("]]", "next", buffer)
+      map("[[", "prev", buffer)
+    end,
+  })
 end
+
+M.keys = {
+  { "]]", desc = "Next Reference" },
+  { "[[", desc = "Prev Reference" },
+}
 
 return M
