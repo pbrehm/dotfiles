c0d8d4b7e472499026befb8c13c4aab14d7dbcb6
pbrehm <peter.brehm@outlook.com>
2024-02-22 12:07:22 -0600

    nvim: update lsp wk to use function instead of <cmd>lua

--- nvim/lua/user/lspconfig.lua
+++ nvim/lua/user/lspconfig.lua
@@ -3,7 +3,7 @@ local M = {
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
     "folke/neodev.nvim",
-    "yioneko/nvim-vtsls"
+    "yioneko/nvim-vtsls",
   },
 }
 
@@ -38,10 +38,10 @@ M.toggle_inlay_hints = function()
 end
 
 function M.config()
-  require("lspconfig.configs").vtsls = require('vtsls').lspconfig
+  require("lspconfig.configs").vtsls = require("vtsls").lspconfig
   local wk = require "which-key"
   wk.register {
-    ["<leader>ca"] = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+    ["<leader>ca"] = { function() vim.lsp.buf.code_action() end, "Code Action" },
     -- formatter keybind added to conform
     -- ["<leader>cf"] = {
     --   "<cmd>lua vim.lsp.buf.format({async = true, filter = function(client) return client.name ~= 'typescript-tools' end})<cr>",
@@ -49,17 +49,16 @@ function M.config()
     -- },
     ["<leader>ci"] = { "<cmd>LspInfo<cr>", "Info" },
     -- ["<leader>lj"] = { "<cmd>lua vim.diagnostic.goto_next()<cr>", "Next Diagnostic" },
-    ["<leader>uh"] = { "<cmd>lua require('user.lspconfig').toggle_inlay_hints()<cr>", "Hints" },
+    ["<leader>uh"] = { function() require('user.lspconfig').toggle_inlay_hints() end, "Hints" },
     -- ["<leader>lk"] = { "<cmd>lua vim.diagnostic.goto_prev()<cr>", "Prev Diagnostic" },
-    ["<leader>cA"] = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
+    ["<leader>cA"] = { function() vim.lsp.codelens.run() end, "CodeLens Action" },
     -- ["<leader>lq"] = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Quickfix" },
-    ["<leader>cr"] = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
+    ["<leader>cr"] = { function() vim.lsp.buf.rename() end, "Rename" },
     ["<leader>co"] = { "<cmd>VtsExec organize_imports<cr>", "Organize Imports (vtsls)" },
     ["<leader>cR"] = { "<cmd>VtsExec remove_unused_imports<cr>", "Remove Unused Imports (vtsls)" },
   }
 
-  wk.register {
-    ["<leader>ca"] = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action", mode = "v" },
+  wk.register { ["<leader>ca"] = { function() vim.lsp.buf.code_action() end, "Code Action", mode = "v", },
   }
 
   local lspconfig = require "lspconfig"
@@ -79,9 +78,9 @@ function M.config()
       active = true,
       values = {
         { name = "DiagnosticSignError", text = icons.diagnostics.BoldError },
-        { name = "DiagnosticSignWarn",  text = icons.diagnostics.BoldWarning },
-        { name = "DiagnosticSignHint",  text = icons.diagnostics.BoldHint },
-        { name = "DiagnosticSignInfo",  text = icons.diagnostics.BoldInformation },
+        { name = "DiagnosticSignWarn", text = icons.diagnostics.BoldWarning },
+        { name = "DiagnosticSignHint", text = icons.diagnostics.BoldHint },
+        { name = "DiagnosticSignInfo", text = icons.diagnostics.BoldInformation },
       },
     },
     virtual_text = true,
@@ -101,7 +100,10 @@ function M.config()
   vim.diagnostic.config(default_diagnostic_config)
 
   for _, sign in ipairs(vim.tbl_get(vim.diagnostic.config(), "signs", "values") or {}) do
-    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, --[[ numhl = sign.name ]] })
+    vim.fn.sign_define(sign.name, {
+      texthl = sign.name,
+      text = sign.text, --[[ numhl = sign.name ]]
+    })
   end
 
   -- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
