52c2e9fcae6bd33cb692db1a474de27708e81936
pbrehm <peter.brehm@outlook.com>
2024-02-26 10:35:22 -0600

    nvim: switch navigation to alt hjkl

--- nvim/lua/user/config/keymaps.lua
+++ nvim/lua/user/config/keymaps.lua
@@ -6,11 +6,11 @@ map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent =
 map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
--- Move to window using the <ctrl> hjkl keys
-map("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
-map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
-map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
-map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
+-- Move to window using the <alt> hjkl keys
+map("n", "<M-h>", "<C-w>h", { desc = "Go to left window", remap = true })
+map("n", "<M-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
+map("n", "<M-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
+map("n", "<M-l>", "<C-w>l", { desc = "Go to right window", remap = true })
 
 -- Resize window using <ctrl> arrow keys
 map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
