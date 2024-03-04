098c503c96b3309afafcb277603441aeb6e6d8be
pbrehm <peter.brehm@outlook.com>
2024-03-04 13:49:49 -0600

    nvim: cmp update keymaps from new kickstart config

--- nvim/lua/user/cmp.lua
+++ nvim/lua/user/cmp.lua
@@ -42,7 +42,7 @@ local M = {
 function M.config()
   local cmp = require "cmp"
   local luasnip = require "luasnip"
-  require("luasnip.loaders.from_vscode").lazy_load({ paths = "./snippets/vscode-jest-snippets-custom" })
+  require("luasnip.loaders.from_vscode").lazy_load { paths = "./snippets/vscode-jest-snippets-custom" }
 
   vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
   vim.api.nvim_set_hl(0, "CmpItemKindTabnine", { fg = "#CA42F0" })
@@ -56,23 +56,59 @@ function M.config()
         luasnip.lsp_expand(args.body) -- For `luasnip` users.
       end,
     },
-    mapping = cmp.mapping.preset.insert({
-      ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-      ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ["<C-Space>"] = cmp.mapping.complete(),
-      ["<C-e>"] = cmp.mapping.abort(),
-      ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      ["<S-CR>"] = cmp.mapping.confirm({
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = true,
-      }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      ["<C-CR>"] = function(fallback)
-        cmp.abort()
-        fallback()
-      end,
-    }),
+    -- mapping = cmp.mapping.preset.insert({
+    --   ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+    --   ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+    --   ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    --   ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    --   ["<C-Space>"] = cmp.mapping.complete(),
+    --   ["<C-e>"] = cmp.mapping.abort(),
+    --   ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    --   ["<S-CR>"] = cmp.mapping.confirm({
+    --     behavior = cmp.ConfirmBehavior.Replace,
+    --     select = true,
+    --   }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    --   ["<C-CR>"] = function(fallback)
+    --     cmp.abort()
+    --     fallback()
+    --   end,
+    -- }),
+
+    mapping = cmp.mapping.preset.insert {
+      -- Select the [n]ext item
+      ["<C-n>"] = cmp.mapping.select_next_item(),
+      -- Select the [p]revious item
+      ["<C-p>"] = cmp.mapping.select_prev_item(),
+
+      -- Accept ([y]es) the completion.
+      --  This will auto-import if your LSP supports it.
+      --  This will expand snippets if the LSP sent a snippet.
+      ["<C-y>"] = cmp.mapping.confirm { select = true },
+
+      -- Manually trigger a completion from nvim-cmp.
+      --  Generally you don't need this, because nvim-cmp will display
+      --  completions whenever it has completion options available.
+      ["<C-Space>"] = cmp.mapping.complete {},
+
+      -- Think of <c-l> as moving to the right of your snippet expansion.
+      --  So if you have a snippet that's like:
+      --  function $name($args)
+      --    $body
+      --  end
+      --
+      -- <c-l> will move you to the right of each of the expansion locations.
+      -- <c-h> is similar, except moving you backwards.
+      ["<C-l>"] = cmp.mapping(function()
+        if luasnip.expand_or_locally_jumpable() then
+          luasnip.expand_or_jump()
+        end
+      end, { "i", "s" }),
+      ["<C-h>"] = cmp.mapping(function()
+        if luasnip.locally_jumpable(-1) then
+          luasnip.jump(-1)
+        end
+      end, { "i", "s" }),
+    },
     formatting = {
       fields = { "kind", "abbr", "menu" },
       format = function(entry, vim_item)
