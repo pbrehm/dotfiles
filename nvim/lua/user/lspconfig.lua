f4238bb04585c08d0d1907ccdac879b0c36c4c19
pbrehm <peter.brehm@outlook.com>
2024-03-07 13:56:32 -0600

    nvim: add tsc plugin plus keymap to pull project wide ts errors to quickfix menu

--- nvim/lua/user/lspconfig.lua
+++ nvim/lua/user/lspconfig.lua
@@ -4,6 +4,17 @@ local M = {
   dependencies = {
     "folke/neodev.nvim",
     "yioneko/nvim-vtsls",
+    {
+      "dmmulroy/tsc.nvim",
+      lazy = true,
+      ft = { "typescript", "typescriptreact" },
+      config = function()
+        require("tsc").setup {
+          auto_open_qflist = true,
+          pretty_errors = false,
+        }
+      end,
+    },
   },
 }
 
@@ -13,8 +24,10 @@ local function lsp_keymaps(bufnr)
   keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
   keymap(bufnr, "n", "gd", '<cmd>lua require("telescope.builtin").lsp_definitions({ reuse_win = true })<CR>', opts)
   keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  keymap(bufnr, "n", "<C-h>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
   keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
   keymap(bufnr, "n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
+  keymap(bufnr, "n", "<leader>cc", ":TSC<cr>", opts)
   -- keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
 end
 
@@ -51,7 +64,13 @@ function M.config()
   require("lspconfig.configs").vtsls = require("vtsls").lspconfig
   local wk = require "which-key"
   wk.register {
-    ["<leader>ca"] = { function() vim.lsp.buf.code_action() end, "Code Action", mode = {"n", "v"} },
+    ["<leader>ca"] = {
+      function()
+        vim.lsp.buf.code_action()
+      end,
+      "Code Action",
+      mode = { "n", "v" },
+    },
     -- formatter keybind added to conform
     -- ["<leader>cf"] = {
     --   "<cmd>lua vim.lsp.buf.format({async = true, filter = function(client) return client.name ~= 'typescript-tools' end})<cr>",
@@ -59,11 +78,26 @@ function M.config()
     -- },
     ["<leader>ci"] = { "<cmd>LspInfo<cr>", "Info" },
     -- ["<leader>lj"] = { "<cmd>lua vim.diagnostic.goto_next()<cr>", "Next Diagnostic" },
-    ["<leader>uh"] = { function() require('user.lspconfig').toggle_inlay_hints() end, "Hints" },
+    ["<leader>uh"] = {
+      function()
+        require("user.lspconfig").toggle_inlay_hints()
+      end,
+      "Hints",
+    },
     -- ["<leader>lk"] = { "<cmd>lua vim.diagnostic.goto_prev()<cr>", "Prev Diagnostic" },
-    ["<leader>cA"] = { function() vim.lsp.codelens.run() end, "CodeLens Action" },
+    ["<leader>cA"] = {
+      function()
+        vim.lsp.codelens.run()
+      end,
+      "CodeLens Action",
+    },
     -- ["<leader>lq"] = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Quickfix" },
-    ["<leader>cr"] = { function() vim.lsp.buf.rename() end, "Rename" },
+    ["<leader>cr"] = {
+      function()
+        vim.lsp.buf.rename()
+      end,
+      "Rename",
+    },
     ["<leader>co"] = { "<cmd>VtsExec organize_imports<cr>", "Organize Imports (vtsls)" },
     ["<leader>cR"] = { "<cmd>VtsExec remove_unused_imports<cr>", "Remove Unused Imports (vtsls)" },
   }
