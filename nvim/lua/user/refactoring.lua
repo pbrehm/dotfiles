59123f404e4967ef71b62e9f446bacb37e0f6d24
pbrehm <peter.brehm@outlook.com>
2024-03-07 21:27:54 -0600

    nvim: refactor add <leader>rr to open refactor menu

--- nvim/lua/user/refactoring.lua
+++ nvim/lua/user/refactoring.lua
@@ -8,67 +8,74 @@ local M = {
 }
 
 function M.config()
-    require("refactoring").setup()
+  require("refactoring").setup()
 end
 
 M.keys = {
-    {
-      "<leader>re",
-      function()
-        require("refactoring").refactor("Extract Function")
-      end,
-      mode = { "x" },
-      desc = "Extract Function",
-    },
-    {
-      "<leader>rf",
-      function()
-        require("refactoring").refactor("Extract Function To File")
-      end,
-      mode = { "x" },
-      desc = "Extract Function To File",
-    },
-    {
-      "<leader>rv",
-      function()
-        require("refactoring").refactor("Extract Variable")
-      end,
-      mode = { "x" },
-      desc = "Extract Variable",
-    },
-    {
-      "<leader>rI",
-      function()
-        require("refactoring").refactor("Inline Function")
-      end,
-      mode = { "n" },
-      desc = "Inline Function",
-    },
-    {
-      "<leader>ri",
-      function()
-        require("refactoring").refactor("Inline Variable")
-      end,
-      mode = { "n", "x" },
-      desc = "Inline Variable",
-    },
-    {
-      "<leader>rb",
-      function()
-        require("refactoring").refactor("Extract Block")
-      end,
-      mode = "n",
-      desc = "Extract Block",
-    },
-    {
-      "<leader>rbf",
-      function()
-        require("refactoring").refactor("Extract Block To File")
-      end,
-      mode = "n",
-      desc = "Extract Block To File",
-    },
-  }
+  {
+    "<leader>rr",
+    function()
+      require("refactoring").select_refactor()
+    end,
+    mode = { "n", "x" },
+    desc = "Refactor Menu",
+  },
+  {
+    "<leader>re",
+    function()
+      require("refactoring").refactor "Extract Function"
+    end,
+    mode = { "x" },
+    desc = "Extract Function",
+  },
+  {
+    "<leader>rf",
+    function()
+      require("refactoring").refactor "Extract Function To File"
+    end,
+    mode = { "x" },
+    desc = "Extract Function To File",
+  },
+  {
+    "<leader>rv",
+    function()
+      require("refactoring").refactor "Extract Variable"
+    end,
+    mode = { "x" },
+    desc = "Extract Variable",
+  },
+  {
+    "<leader>rI",
+    function()
+      require("refactoring").refactor "Inline Function"
+    end,
+    mode = { "n" },
+    desc = "Inline Function",
+  },
+  {
+    "<leader>ri",
+    function()
+      require("refactoring").refactor "Inline Variable"
+    end,
+    mode = { "n", "x" },
+    desc = "Inline Variable",
+  },
+  {
+    "<leader>rb",
+    function()
+      require("refactoring").refactor "Extract Block"
+    end,
+    mode = "n",
+    desc = "Extract Block",
+  },
+  {
+    "<leader>rbf",
+    function()
+      require("refactoring").refactor "Extract Block To File"
+    end,
+    mode = "n",
+    desc = "Extract Block To File",
+  },
+}
 
 return M
-
