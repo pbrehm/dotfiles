e6924deb6116a4dbe9dbd4ac83214f0885e2ecfd
pbrehm <peter.brehm@outlook.com>
2024-03-07 21:28:10 -0600

    nvim: add open and close quickfix bind

--- nvim/lua/user/config/keymaps.lua
+++ nvim/lua/user/config/keymaps.lua
@@ -108,6 +108,8 @@ map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
 map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
 map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
 
+map("n", "<leader>q", "<cmd>copen<cr>")
+map("n", "<leader>Q", "<cmd>cclose<cr>")
 -- stylua: ignore start
 
 -- toggle options
