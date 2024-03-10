4a3ec3f99b1d28903ad04a101d3accc9ca6c5aa4
pbrehm <peter.brehm@outlook.com>
2024-03-10 15:54:22 -0500

    nvim: persistence change keymaps back to q<key>

--- nvim/lua/user/persistence.lua
+++ nvim/lua/user/persistence.lua
@@ -4,9 +4,9 @@ local M = {
   opts = { options = vim.opt.sessionoptions:get() },
     -- stylua: ignore
     keys = {
-      { "<leader>Rs", function() require("persistence").load() end, desc = "Restore Session" },
-      { "<leader>Rl", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
-      { "<leader>Rd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+      { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
+      { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+      { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
     },
 }
 
