faf013d52025faabeb82c7ccbfb834d8c5cfdecc
pbrehm <peter.brehm@outlook.com>
2024-03-20 14:52:57 -0500

    nvim: enable inlay hints for lua_ls

--- nvim/lua/user/lspsettings/lua_ls.lua
+++ nvim/lua/user/lspsettings/lua_ls.lua
@@ -22,10 +22,10 @@ return {
         },
       },
       hint = {
-        enable = false,
-        arrayIndex = "Disable", -- "Enable" | "Auto" | "Disable"
+        enable = true,
+        arrayIndex = "Enable", -- "Enable" | "Auto" | "Disable"
         await = true,
-        paramName = "Disable", -- "All" | "Literal" | "Disable"
+        paramName = "All", -- "All" | "Literal" | "Disable"
         paramType = true,
         semicolon = "All", -- "All" | "SameLine" | "Disable"
         setType = false,
