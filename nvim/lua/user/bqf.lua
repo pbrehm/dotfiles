2e40f4139d63356c6410b98b023dbd76cf8040cb
pbrehm <peter.brehm@outlook.com>
2024-09-24 17:03:20 -0500

    nvim: add fzf dependency for bqf

--- nvim/lua/user/bqf.lua
+++ nvim/lua/user/bqf.lua
@@ -1,5 +1,10 @@
 local M = {
   "kevinhwang91/nvim-bqf",
+
+  dependencies = {
+    "junegunn/fzf", -- needed to use fzf mode. (zf keybin)
+    build = ":call fzf#install()",
+  },
 }
 
 function M.config()
