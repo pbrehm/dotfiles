ffc86c6e393a27e22738318d598589a538d4a632
pbrehm <peter.brehm@outlook.com>
2024-10-04 16:23:35 -0500

    nvim: revert trouble upgrade
    Having issue where when test run is finished screen is frozen.
    Reverting todo trouble update since it seemed to not happen when I
    checked out revision before that
    
    Revert "nvim: fix todo-trouble for custom pb tag and cleanup old commit
    hashes etc."
    
    This reverts commit 5f5e0f79880a470f7cdfc1d46f22f04f837bd82e.
    
    Revert "nvim: use new version of trouble but disable keybind to open
    todos in trouble since it gives error"
    
    This reverts commit a27feae00370b21d14f618a048c40c30ef5f6b4a.

--- nvim/lua/user/todo-comments.lua
+++ nvim/lua/user/todo-comments.lua
@@ -3,6 +3,7 @@ local tagsCommand = "keywords=" .. tags
 
 local M = {
   "folke/todo-comments.nvim",
+  commit = "a7e39ae9e74f2c8c6dc4eea6d40c3971ae84752d",
   cmd = { "TodoTrouble", "TodoTelescope" },
   config = true,
   event = "BufEnter",
@@ -11,15 +12,19 @@ local M = {
     keywords = {
       PB = {
         color = "info",
-        icon = "",
       },
+
     },
   },
+    -- stylua: ignore
     keys = {
       { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
       { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
-      { "<leader>xt", "<cmd>Trouble todo toggle filter = {tag = {"..tags.."}}<cr>", desc = tags.." (Trouble)" },
+      -- { "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Todo (Trouble)" },
+      { "<leader>xt", "<cmd>TodoTrouble "..tagsCommand.."<cr>", desc = tags.." (Trouble)" },
       { "<leader>st", "<cmd>TodoTelescope "..tagsCommand.."<cr>", desc = tags.." tags" },
+      -- { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
     },
 }
+
 return M
--- nvim/lua/user/trouble.lua
+++ nvim/lua/user/trouble.lua
@@ -1,11 +1,12 @@
 local M = {
   "folke/trouble.nvim",
+  commit = "a8264a65a0b894832ea642844f5b7c30112c458f", -- PB I do not like the new dark highlight and it broke todo comments. using old commit for now
   dependencies = { "nvim-tree/nvim-web-devicons" },
   keys = {
-    { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
-    { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
-    { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
-    { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
+    { "<leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
+    { "<leader>xX", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
+    { "<leader>xL", "<cmd>TroubleToggle loclist<cr>", desc = "Location List (Trouble)" },
+    { "<leader>xQ", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List (Trouble)" },
     {
       "[q",
       function()
