137664d42877fe54bcf0adb0c3a6511b2edb2aee
pbrehm <peter.brehm@outlook.com>
2024-11-22 10:55:44 -0600

    nvim: oil, update keybind to not open in floating window

--- nvim/lua/user/oil.lua
+++ nvim/lua/user/oil.lua
@@ -17,7 +17,8 @@ function M.config()
       max_width = 60,
     },
   }
-  vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+  -- vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+  vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 end
 
 return M
